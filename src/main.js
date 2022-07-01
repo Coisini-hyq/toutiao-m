43d1e73de206bf9a935b0e385871f895fbb127d9
Coisini-hyq <1519392320@qq.com>
2022-07-01 14:26:30 +0800

    项目初始化-调整初始目录结构

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import router from '@/router'
+
+//引入全局样式
+import './styles/index.less'
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    router,
+    render: h => h(App),
 }).$mount('#app')
